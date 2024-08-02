511e92b75aaf72be6be9db1a0b030acd40ce7b96
Abduxabir2024 <abduqayumovabduxabir@gmail.com>
2024-08-02 22:58:54 +0500

    mklop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Abaut from "./page/Abduxabir/abaut";
 
 
 export default function App() {
   return (
     <>
-    
+    <Routes>
+      <Route path="/abaut"  element={<Abaut/>} />
+    </Routes>
     </>
   )
 }
---
+++ src/axios/axios.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const url = axios.create({
+  baseURL: "https://crudcrud.com/api/2144d5fd47a74012ace9e2bb7fad072c",
+});
