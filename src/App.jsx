f0184e4abb72d00a5fff9135c25d55c8c014065c
jamshid0-dev <jamshidt234@gmail.com>
2024-08-04 15:41:40 +0500

    jamshiddd

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Abaut from "./page/Abduxabir/abaut";
 import Homeg from "./page/Abdulaziz/abdulaziz"
 import Navbar from "./campanent/navbar/navbar";
 import Login from "./page/Shoxzamon/shoxzamon";
-import Footerr from "./campanent/footer/footer";
+import { Footer } from "./campanent/footer/footer";
 import ContactUser from "./page/jashid/jamshid";
 
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
       <Route path="/RegisteUser"  element={<ContactUser/>} />
       <Route path="/Login" element={<Login/>} />
     </Routes>
-    <Footerr />
+    <Footer />
     </>
   )
 }
--- src/campanent/footer/footer.jsx
+++ src/campanent/footer/footer.jsx
@@ -1,9 +1,56 @@
-import "./Footer.scss"
+import { Typography } from "@material-tailwind/react";
+import { useTranslation } from "react-i18next";
 
-export default function Footerr(){
-    return(
-        <>
-        <h1>Footer</h1>
-        </>
-    )
+export function Footer() {
+    const { t } = useTranslation();
+    return (
+        <footer className=" px-[70px] flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
+            <Typography color="blue-gray" className="font-normal flex items-center">
+                 <img src="https://img.freepik.com/premium-vector/gw-logo_745848-61.jpg" alt="" className="w-20" />
+                <h1>GRIDWORK</h1>
+            </Typography>
+            <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+                <li>
+                    <Typography
+                        as="a"
+                        href="#"
+                        color="blue-gray"
+                        className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                    >
+                        {t("home")}
+                    </Typography>
+                </li>
+                <li>
+                    <Typography
+                        as="a"
+                        href="#"
+                        color="blue-gray"
+                        className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                    >
+                        {t("about")}
+                    </Typography>
+                </li>
+                <li>
+                    <Typography
+                        as="a"
+                        href="#"
+                        color="blue-gray"
+                        className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                    >
+                        {t("reg")}
+                    </Typography>
+                </li>
+                <li>
+                    <Typography
+                        as="a"
+                        href="#"
+                        color="blue-gray"
+                        className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                    >
+                        {t("Log In")}
+                    </Typography>
+                </li>
+            </ul>
+        </footer>
+    );
 }
